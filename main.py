02978e3921287b759f5595d33e92d1509cdfaf4e
Joo HyeonJin <icepri3535@gmail.com>
2022-12-02 15:31:34 +0900

    3ed js, css 테스트

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# main
+@app.route('/')
+def root():
+    return render_template('main.html')
+
+
+# 서버실행
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 5000)
