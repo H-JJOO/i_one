d19a279c7fcef35fd26de54b64068effb4cdf90e
Joo HyeonJin <icepri3535@gmail.com>
2022-12-06 01:20:33 +0900

    9th 회원가입 로그인 기능 대충 완료

--- main.py
+++ main.py
@@ -1,62 +1,105 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import pymysql
+import js2py
 import json
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "abcd"
 
 
 # main
 @app.route('/')
-def root():
-    return render_template('main.html')
+def home():
+    if "usernm" in session:
+        return render_template('main.html', usernm = session.get("usernm"), login = True)
+    else:
+        return render_template('main.html', login = False)
 
 
 # login
-@app.route('/login')
+@app.route('/users/login', methods = ['GET', 'POST'])
 def login():
+    db = pymysql.connect(host = 'localhost', user = 'root', db = 'i_one', password = 'M@ansghkwo12', charset = 'utf8')
+    curs = db.cursor()
+
+    if request.method == 'POST':
+        uid = request.form['userId']
+        upw = request.form['password']
+
+        print(uid, upw)
+
+        curs.execute("SELECT * FROM t_user")
+
+        t_user_list = curs.fetchall()
+
+        print(t_user_list)
+
+        for t_user in t_user_list:
+            print(t_user)
+            if uid == t_user[1]:
+                if upw == t_user[2]:
+                    session["usernm"] = t_user[3]
+                    return redirect("/")
+                else:
+                    return '<script>alert("비밀번호가 틀렸습니다."); document.location.href="login"; </script>'
+        return '<script>alert("아이디가 틀렸습니다."); document.location.href="login"; </script>'
+
+
+        db.commit()
+        db.close()
+
     return render_template('login.html')
 
 
 # sign up
-@app.route('/signup')
+@app.route('/users/signup')
 def signup():
     return render_template('signup.html')
 
-#마이페이지
+
+# 마이페이지
 @app.route('/mypage')
 def mypage():
     return render_template('mypage.html')
 
-#프로필 수정 페이지
+
+# 프로필 수정 페이지
 @app.route('/setting')
 def mypageSetting():
     return render_template('mypage_setting.html')
 
+
 # sign up, INSERT
-@app.route('/signup', methods = ['POST', 'GET'])
+@app.route('/users/signup', methods = ['POST'])
 def insert_user():
-    db = pymysql.connect(host = 'localhost', user = 'root', db = 'mysql', password = 'M@ansghkwo12', charset = 'utf8')
+    db = pymysql.connect(host = 'localhost', user = 'root', db = 'i_one', password = 'M@ansghkwo12', charset = 'utf8')
     curs = db.cursor()
 
     if request.method == 'POST':
-        uid = request.form['name']
+        uid = request.form['userId']
         upw = request.form['password']
         nm = request.form['name']
         gender = request.form['gender']
-        email = request.form['email']
+        email = request.form["email"]
         loc = request.form['location']
 
         sql = """insert into t_user (uid, upw, nm, gender, email, location)
          values (%s,%s,%s,%s,%s,%s)
         """
-        curs.execute(sql, (uid, upw, nm, gender))
+        curs.execute(sql, (uid, upw, nm, gender, email, loc))
 
-        return 'insert success', 200
+        db.commit()
+        db.close()
 
-    user = request.json
+        return render_template('main.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop("usernm")
+    return redirect("/")
 
 
 # 서버실행
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000,debug=True)
-
+    app.run(host = '127.0.0.1', port = 5000, debug = True)
--- static/javascript/signup.js
+++ static/javascript/signup.js
@@ -1,3 +1,4 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
